5a942e76e8ffbdc467918907beab82872daf35e9
shtashev <sherruh@gmail.com>
2023-05-24 20:38:28 +0600

    Draft

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import random
+from random import randint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+num_list = [1,8,2,5,0]#[random.randint(1,100) for i in range(50)]
+num_list_sorted = list()
+
+print(num_list)
+
+min_num = num_list[0]
+index_of_min_num = 0
+max_num = num_list[-1]
+index_of_max_num = len(num_list) - 1
+stop = len(num_list)
+while len(num_list) > 0:
+    for i in range(0, stop):
+        if min_num > num_list[i]:
+            min_num = num_list[i]
+            index_of_min_num = i
+            if stop - 1 == i:
+                stop -= 1
+                num_list_sorted.append(min_num)
+                num_list.pop(index_of_min_num)
+                min_num = num_list[0]
+                index_of_min_num = 0
+                continue
+        if stop - 1 == i:
+            stop -= 1
+            num_list_sorted.insert(len(num_list_sorted),min_num)
+            num_list.pop(index_of_min_num)
+            index_of_min_num = 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+print(num_list_sorted)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    #for j in range(i + 1,stop - 1):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
